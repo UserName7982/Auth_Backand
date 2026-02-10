256f64f1258f9c4196b7f596d71e988e38c0c5c9
Harsh <mh116727@gmail.com>
2026-02-10 20:28:46 +0530

    Adding File

--- src/__init__.py
+++ src/__init__.py
@@ -6,6 +6,9 @@ from contextlib import asynccontextmanager
 from src.DB.Redis import redis
 from src.middleware import add_process_time_header
 from starlette.middleware.base import BaseHTTPMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+
+
 version="1.0.1"
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -15,3 +18,5 @@ async def lifespan(app: FastAPI):
 app=FastAPI(version=version,description="AI Assistant",lifespan=lifespan)
 app.add_middleware(BaseHTTPMiddleware, dispatch=add_process_time_header)
 app.include_router(auth_router,prefix=f"/api/{version}",tags=["auth"])
+
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=["localhost", "127.0.0.1","https://auth-backand.onrender.com"])
